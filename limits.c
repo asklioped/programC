a0b8ff72e03abd9c6a1cb50bc234904529217327
asklioped <33459959+asklioped@users.noreply.github.com>
2023-05-12 14:12:38 +0300

    Create limits.c

---
+++ limits.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <limits.h>
+//#include <float.h>
+
+int main(void){
+    printf("Найбільше значення short int для нашої системи - %d\n", SHRT_MAX );
+    printf("Найменше значення short int для нашої системи - %d\n", SHRT_MAX);
+    printf("Найбільше значення int для нашої системи - %d\n", INT_MAX);
+    printf("Найменше значення int для нашої системи - %d\n", INT_MIN);
+    printf("%d\n", UINT_MAX);
+    
+    return 0;
+}
